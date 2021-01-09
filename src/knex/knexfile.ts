b8f026ab7307525380022d8f5d4903c0c964553b
Dylan O'Leary <dylaneamusic@gmail.com>
2021-01-09 17:59:07 -0500

    Add docker-compose file

--- src/knex/knexfile.ts
+++ src/knex/knexfile.ts
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/../../.env" });
 const path = require("path");
 
 module.exports = {
@@ -17,11 +17,11 @@ module.exports = {
     development: {
         client: "mysql2",
         connection: {
-            host: "localhost",
-            port: 3306,
-            user: "admin",
-            password: "frogger",
-            database: "songstagram"
+            host: process.env.MYSQL_HOST,
+            port: process.env.MYSQL_PORT,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE
         },
         migrations: { directory: "./migrations" },
         seeds: { directory: "./seeds" }
